81df22bc5f25a5fad7d18b8bad9dcf358e1ccfd0
itsspall <naufalnurmaryadi18@gmail.com>
2025-06-13 09:37:45 +0700

    fixing bug kupon

--- body/kupon.c
+++ body/kupon.c
@@ -1,5 +1,7 @@
-#include "kupon.h"
+#include "../header/kupon.h"
 #include <stdio.h>
+
+int kupon[100];
 
 void tampilkanKupon(const char* jenisMobil, int idKupon) {
     printf("\n==================== KUPON CUCI MOBIL ====================\n");
